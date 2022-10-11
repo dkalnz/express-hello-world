aa7b37c6cb1d09c76008f873f6c65b7dd2a4e980
Danny K <dxkalnz@gmail.com>
2022-10-11 15:08:27 -0600

    Commit after lunch

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //access request object, access response object
 app.get('/', (request, response) => {
 	//callback string 'Hello World'
 	response.send('Hello World');
 });
 
+// Default Route
+// What
+app.post('/', (req, res) => {
+	res.send(`hello ${req.body.name}`);
+});
+
+app.get('/year/:yr/:name', (req, res) => {
+	console.log(req.params);
+	res.send(`Hello ${req.params.name} it's ${req.params.yr}`);
+	// hello cora its 1990
+});
+
+app.post('/', (req, res) => {
+	res.send('hello from the post route');
+});
+
 app.listen(4000, () => {
 	console.log('app listening on port 4000');
 });
+//RUN ' nodemon index.js ' TO START
+// ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^
+//
+//
---
+++ moviesData.js
@@ -0,0 +1,62 @@
+// moviesData.js
+
+const movies = [
+	{
+		id: 181812,
+		video: 'https://www.youtube.com/embed/8Qn_spdM5Zg',
+		title: 'Star Wars: The Rise of Skywalker',
+		release_date: '2019-12-20',
+		poster: 'https://image.tmdb.org/t/p/w500/db32LaOibwEliAmSL2jjDF6oDdj.jpg',
+		image: 'https://image.tmdb.org/t/p/w1280/jOzrELAzFxtMx2I4uDGHOotdfsS.jpg',
+		rotten_tomatoes: 53,
+		audience_score: 86,
+		summary:
+			'The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. With the power and knowledge of generations behind them, the final battle begins.',
+	},
+	{
+		id: 530915,
+		title: '1917',
+		release_date: '2019-12-10',
+		poster: 'https://image.tmdb.org/t/p/w500/Aqr1jDwGb1IfAB6Lh1fNx7AbEZE.jpg',
+		image: 'https://image.tmdb.org/t/p/w1280/tUWivz05fcY14K6RzicRm7IHkUD.jpg',
+		rotten_tomatoes: 90,
+		audience_score: 90,
+		summary:
+			"At the height of the First World War, two young British soldiers, Schofield and Blake are given a seemingly impossible mission. In a race against time, they must cross enemy territory and deliver a message that will stop a deadly attack on hundreds of soldiers—Blake's own brother among them.",
+	},
+	{
+		id: 431693,
+		title: 'Spies in Disguise',
+		release_date: '2019-12-25',
+		poster: 'https://image.tmdb.org/t/p/w500/30YacPAcxpNemhhwX0PVUl9pVA3.jpg',
+		image: 'https://image.tmdb.org/t/p/w1280/qlYxtqVfu2LOdvYMMDPCSGX0Oz0.jpg',
+		rotten_tomatoes: 76,
+		audience_score: 91,
+		summary:
+			"When the world's best spy is turned into a pigeon, he must rely on his nerdy tech officer to save the world.",
+	},
+	{
+		id: 331482,
+		title: 'Little Women',
+		release_date: '2019-12-25',
+		poster: 'https://image.tmdb.org/t/p/w500/mSmiB8XjUnR1GSIljuCPGsk0cwX.jpg',
+		image: 'https://image.tmdb.org/t/p/w1280/3uTxPIdVEXxHpsHOHdJC24QebBV.jpg',
+		rotten_tomatoes: 95,
+		audience_score: 92,
+		summary:
+			'Four sisters come of age in America in the aftermath of the Civil War.',
+	},
+	{
+		id: 366668,
+		title: 'Playmobil: The Movie',
+		release_date: '2019-12-06',
+		poster: 'https://image.tmdb.org/t/p/w500/zPQzLZnfVw9fbXyxxglyOsmQBlu.jpg',
+		image: 'https://image.tmdb.org/t/p/w1280/axmxVeX5R1FfN7w4LNFehskDxW1.jpg',
+		rotten_tomatoes: 17,
+		audience_score: 61,
+		summary:
+			'Marla is forced to abandon her carefully structured life to embark on an epic journey to find her younger brother Charlie who has disappeared into the vast and wondrous animated world of Playmobil toys.',
+	},
+];
+
+module.exports = movies;
