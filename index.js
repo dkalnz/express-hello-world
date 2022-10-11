e58b40db240749d9f0f33e0499768039ae7d8a08
Danny K <dxkalnz@gmail.com>
2022-10-11 13:10:58 -0600

    Add basic hello world GET

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+//access request object, access response object
+app.get('/', (request, response) => {
+	//callback string 'Hello World'
+	response.send('Hello World');
+});
+
 app.listen(4000, () => {
 	console.log('app listening on port 4000');
 });
